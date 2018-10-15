61fee56a87c1570f75614ae29df8d1fb19f7eddb
8eecf0d2 <8eecf0d23d248b0e6223b8a10ddbf1@gmail.com>
2018-10-16 08:56:53 +1100

    💫 added explicit Config, Netlify, Server, Webpack exports #6

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,4 @@
-export * from "./netlify";
+export { parseNetlifyConfig, parseWebpackConfig } from "./config";
+export { Netlify } from "./netlify";
+export { Server } from "./server";
+export { Webpack } from "./webpack";
