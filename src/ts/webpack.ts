5afe43f97f02046d35733c710d85fa0679847ac9
8eecf0d2 <8eecf0d23d248b0e6223b8a10ddbf1@gmail.com>
2018-10-16 08:53:35 +1100

    💫 added Webpack build method, increased loggin #6 #7

--- src/ts/webpack.ts
+++ src/ts/webpack.ts
@@ -13,10 +13,31 @@ export class Webpack {
     this.compiler = webpack(this.config);
   }
 
+  public build (): Promise<any> {
+    return new Promise((resolve, reject) => {
+      console.log("netlify-local: webpack build started");
+      this.compiler.run((error, status) => {
+        if(error) {
+          console.log("netlify-local: webpack build failure");
+          return reject(error);
+        }
+        console.log("netlify-local: webpack build success");
+        return resolve(status);
+      });
+    });
+  }
+
   public watch (): void {
     console.log("netlify-local: webpack watching");
-    this.compiler.watch({}, () => {
-      console.log("netlify-local: webpack rebuilt")
+    this.compiler.watch({}, (error, status) => {
+      if(error) {
+        console.log("netlify-local: webpack build failure");
+        console.error(error);
+
+        return;
+      }
+
+      console.log("netlify-local: webpack build success");
     });
   }
 }
