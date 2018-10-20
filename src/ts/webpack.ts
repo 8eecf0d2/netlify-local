a9de33c84af0aa3fecb76db0b817aea6ca97a923
8eecf0d2 <8eecf0d23d248b0e6223b8a10ddbf1@gmail.com>
2018-10-21 08:31:47 +1100

    💫 added support for multiple webpack configs, improved webpack logging #9

--- src/ts/webpack.ts
+++ src/ts/webpack.ts
@@ -2,46 +2,63 @@ import * as webpack from "webpack";
 import { Logger } from "./helper";
 
 export class Webpack {
-  private compiler: webpack.Compiler;
+  private configs: Webpack.Config[];
+  private compilers: webpack.Compiler[];
 
   constructor(
-    private config: Webpack.Config,
+    config: Webpack.Config | Webpack.Config[],
   ) {
+    this.configs = Array.isArray(config) ? config : [config];
     this.initialize();
   }
 
   public initialize (): void {
-    this.compiler = webpack(this.config);
+    this.compilers = this.configs.map(config => webpack(config));
+    Logger.info(`netlify-local: webpack initialized (${this.compilerNames()})`);
+  }
+
+  private compilerName (compiler: webpack.Compiler, iter: number): string {
+    return compiler.name ? compiler.name : `#${iter + 1}`;
+  }
+
+  private compilerNames (): string {
+    return this.compilers.map((compiler, index) => this.compilerName(compiler, index)).join(", ");
   }
 
   public build (): Promise<webpack.Stats> {
     return new Promise((resolve, reject) => {
-      Logger.info("netlify-local: webpack build started");
-      this.compiler.run((error, stats) => {
-        if(error) {
-          Logger.info("netlify-local: webpack build failure");
-          Logger.error(error);
+      Logger.info(`netlify-local: webpack build started (${this.compilerNames()})`);
+      for(let iter = 0; iter < this.compilers.length; iter++) {
+        const compiler = this.compilers[iter];
+        compiler.run((error, stats) => {
+          if(error) {
+            Logger.info(`netlify-local: webpack build failure (${this.compilerName(compiler, iter)})`);
+            Logger.error(error);
 
-          return reject(error);
-        }
-        Logger.info("netlify-local: webpack build success");
-        return resolve(stats);
-      });
+            return reject(error);
+          }
+          Logger.info(`netlify-local: webpack build success (${this.compilerName(compiler, iter)})`);
+          return resolve(stats);
+        });
+      }
     });
   }
 
   public watch (): void {
-    Logger.info("netlify-local: webpack watching");
-    this.compiler.watch({}, (error, stats) => {
-      if(error) {
-        Logger.info("netlify-local: webpack build failure");
-        Logger.error(error);
+    Logger.info(`netlify-local: webpack watching (${this.compilerNames()})`);
+    for(let iter = 0; iter < this.compilers.length; iter++) {
+      const compiler = this.compilers[iter];
+      compiler.watch({}, (error, stats) => {
+        if(error) {
+          Logger.info(`netlify-local: webpack build failure (${this.compilerName(compiler, iter)})`);
+          Logger.error(error);
 
-        return;
-      }
+          return;
+        }
 
-      Logger.info("netlify-local: webpack build success");
-    });
+        Logger.info(`netlify-local: webpack build success (${this.compilerName(compiler, iter)})`);
+      });
+    }
   }
 }
 
