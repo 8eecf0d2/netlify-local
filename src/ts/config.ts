4cd7b82c39868af55f49cff5956f03426010d722
8eecf0d2 <8eecf0d23d248b0e6223b8a10ddbf1@gmail.com>
2018-10-22 07:03:54 +1100

    💫 added simple parser for netlify-local plugin config #12

--- src/ts/config.ts
+++ src/ts/config.ts
@@ -56,3 +56,71 @@ export const parseNetlifyConfig = (filename: string): Netlify.Config => {
 
   return netlifyConfig;
 }
+
+export const parseNetlifyLocalConfig = (netlifyConfig: Netlify.Config, cliOptions: any): Netlify.Plugins.Local => {
+  const netlifyPluginLocalConfig: Netlify.Plugins.Local = {
+    webpack: {
+      config: undefined,
+    },
+    server: {
+      static: undefined,
+      lambda: undefined,
+      certificates: undefined,
+      port: undefined,
+    },
+    functions: {
+      source: undefined,
+      files: [],
+    },
+  };
+
+  /** Parse config from Netlify configuration plugins */
+  if(netlifyConfig.plugins.local) {
+    if(netlifyConfig.plugins.local.hasOwnProperty("webpack")) {
+      if(netlifyConfig.plugins.local.webpack.hasOwnProperty("config")) {
+        netlifyPluginLocalConfig.webpack.config = netlifyConfig.plugins.local.webpack.config;
+      }
+    }
+    if(netlifyConfig.plugins.local.hasOwnProperty("server")) {
+      if(netlifyConfig.plugins.local.server.hasOwnProperty("static")) {
+        netlifyPluginLocalConfig.server.static = netlifyConfig.plugins.local.server.static;
+      }
+      if(netlifyConfig.plugins.local.server.hasOwnProperty("lambda")) {
+        netlifyPluginLocalConfig.server.lambda = netlifyConfig.plugins.local.server.lambda;
+      }
+      if(netlifyConfig.plugins.local.server.hasOwnProperty("certificates")) {
+        netlifyPluginLocalConfig.server.certificates = netlifyConfig.plugins.local.server.certificates;
+      }
+      if(netlifyConfig.plugins.local.server.hasOwnProperty("port")) {
+        netlifyPluginLocalConfig.server.port = netlifyConfig.plugins.local.server.port;
+      }
+    }
+    if(netlifyConfig.plugins.local.hasOwnProperty("functions")) {
+      if(netlifyConfig.plugins.local.functions.hasOwnProperty("source")) {
+        netlifyPluginLocalConfig.functions.source = netlifyConfig.plugins.local.functions.source;
+      }
+      if(netlifyConfig.plugins.local.functions.hasOwnProperty("files")) {
+        netlifyPluginLocalConfig.functions.files = netlifyConfig.plugins.local.functions.files;
+      }
+    }
+  }
+
+  /** Parse command line options */
+  if(cliOptions.webpack !== undefined) {
+    netlifyPluginLocalConfig.webpack.config = cliOptions.webpack;
+  }
+  if(cliOptions.static !== undefined) {
+    netlifyPluginLocalConfig.server.static = cliOptions.static;
+  }
+  if(cliOptions.lambda !== undefined) {
+    netlifyPluginLocalConfig.server.lambda = cliOptions.lambda;
+  }
+  if(cliOptions.certificates !== undefined) {
+    netlifyPluginLocalConfig.server.certificates = cliOptions.certificates;
+  }
+  if(cliOptions.port !== undefined) {
+    netlifyPluginLocalConfig.server.port = cliOptions.port;
+  }
+
+  return netlifyPluginLocalConfig;
+}
