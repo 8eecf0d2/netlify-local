9f2ba605337e83e0301461ce230241f46ed0bc89
8eecf0d2 <8eecf0d23d248b0e6223b8a10ddbf1@gmail.com>
2018-12-21 07:48:25 +1100

    💫 added initial work on bundle command

---
+++ src/ts/command/bundle.ts
@@ -0,0 +1,18 @@
+import { exec } from "child_process";
+
+import { Logger } from "../helper";
+import { Netlify } from "../netlify";
+import { composeWebpackEntry, composeWebpackOutput, Webpack } from "../utility";
+
+export class Bundle {
+  public static async buildWebpackFunctionsConfig(netlifyConfig: Netlify.Config, webpackConfig: Webpack.Config): Promise<void> {
+    const parsedWebpackConfig = {
+      ...webpackConfig,
+      entry: composeWebpackEntry(netlifyConfig),
+      output: composeWebpackOutput(netlifyConfig),
+    };
+
+    console.log(parsedWebpackConfig)
+  }
+
+}
